949a3c36b5bb156bb32ccf86ab86107b79872c60
Sebastien Canart <kokotchy@gmail.com>
2016-09-11 11:08:19 +0200

    Add stupid day 27

---
+++ java/testing/Solution.java
@@ -0,0 +1,22 @@
+package testing;
+
+/**
+ * Created by kokotchy on 9/11/16.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(6);
+        System.out.println("4 2");
+        System.out.println("-1 0 1 1"); //YES
+        System.out.println("5 2");
+        System.out.println("-1 0 0 0 1"); //NO
+        System.out.println("6 2");
+        System.out.println("-1 0 1 1 1 1"); //YES
+        System.out.println("7 2");
+        System.out.println("-1 0 0 0 0 0 1"); //NO
+        System.out.println("8 2");
+        System.out.println("-1 0 1 1 1 1 1 1 1"); //YES
+        System.out.println("9 2");
+        System.out.println("-1 0 0 0 0 0 0 0 1"); //NO
+    }
+}
